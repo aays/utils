6347bd443277394427d93cc679d14a2b4361915e
Ahmed Hasan <ahmed.hasan@mail.utoronto.ca>
2018-06-30 12:47:13 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+osascript -e 'tell application "Terminal"
+    activate
+    tell application "System Events"
+        keystroke "f" using {control down, command down}
+    end tell
+end tell'
+open -a Evernote.app
+open -a Vivaldi.app
+open -a iTunes.app
+open -a airmail\ 3.app
+open -a last.fm.app
